5e7812abeae93f43ae12aa98ff8565a67a764f6b
larry <22062019@qq.com>
2019-05-09 04:50:46 +0800

    updates algorithm

--- assets/js/index.js
+++ assets/js/index.js
@@ -12,7 +12,7 @@
             width: 400,
             height: 400,
             radius: 200,
-            maxSpeed: 0.05,
+            maxSpeed: 0.075,
         };
 
         let yawSpeed = 0;
@@ -49,15 +49,15 @@
                 pitchSpeed *= 0.98;
             }
             tagPool.forEach((tag)=>{
-                tag.yaw += yawSpeed;
-                tag.pitch += pitchSpeed;
+                tag.doPitch(-pitchSpeed);
+                tag.doYaw(-yawSpeed);
             });
             updateTags();
         }
 
         function loop(){
-            tick();
             requestAnimationFrame(loop);
+            tick();
         }
 
         /**
@@ -75,40 +75,65 @@
                 x: config.width * 0.5,
                 y: config.height * 0.5
             };
+            this.tdPoint = {
+                x: 0,
+                y: 0,
+                z: 0
+            };
             this.radius = config.radius * 0.8;
-            this.pitch = Math.random() * Math.PI * 2;
-            this.roll = 0;
-            this.yaw = Math.random() * Math.PI * 2;
+            this.tdPoint.z = this.radius;
             this.width = node.clientWidth;
             this.height = node.clientHeight;
+
+            let u= Math.random();
+            let v= Math.random();
+            let theta = 2 * Math.PI * u;
+            let phi = Math.acos(2 * v - 1);
+            this.tdPoint.x = Math.sin(theta) * Math.sin(phi) * this.radius;
+            this.tdPoint.y = Math.cos(theta) * Math.sin(phi) * this.radius;
+            this.tdPoint.z = Math.cos(phi) * this.radius;
+
+
         }
+
+        Tag.prototype.doPitch = function(theta){
+            let x = this.tdPoint.x;
+            let y = this.tdPoint.y * Math.cos(theta) - this.tdPoint.z * Math.sin(theta);
+            let z = this.tdPoint.y * Math.sin(theta) + this.tdPoint.z * Math.cos(theta);
+            this.tdPoint.x = x;
+            this.tdPoint.y = y;
+            this.tdPoint.z = z;
+        };
+
+        Tag.prototype.doYaw = function(theta){
+            let x = this.tdPoint.z * Math.sin(theta) + this.tdPoint.x * Math.cos(theta);
+            let y = this.tdPoint.y;
+            let z = this.tdPoint.z * Math.cos(theta) - this.tdPoint.x * Math.sin(theta);
+            this.tdPoint.x = x;
+            this.tdPoint.y = y;
+            this.tdPoint.z = z;
+        };
 
         Tag.prototype.update = function(){
 
             let x;
             let y;
-            let z;
-            let a;
-            let indexZ;
+            let zIndex;
             let opacity;
-            let pitch = this.pitch;
-            let yaw = this.yaw;
 
             this.width = this.node.clientWidth;
             this.height = this.node.clientHeight;
 
-            y = Math.sin(pitch) * this.radius * -1;
-            a = y / Math.tan(pitch);
-            x = Math.sin(yaw) * a;
-            z = x / Math.tan(yaw);
-            indexZ = Math.floor(this.radius - z);
-            opacity = (1 - (indexZ / (this.radius * 2))) * 0.8 + 0.2;
+            x = this.tdPoint.x;
+            y = -this.tdPoint.y;
+            zIndex = Math.floor(this.tdPoint.z + this.radius);
+            opacity = (zIndex / (this.radius * 2)) * 0.75 + 0.25;
 
             this.displayPoint.x = this.originPoint.x + x;
             this.displayPoint.y = this.originPoint.y + y;
             this.node.style.left = this.displayPoint.x - this.width * 0.5 + 'px';
             this.node.style.top = this.displayPoint.y - this.height * 0.5 + 'px';
-            this.node.style.zIndex = this.radius * 2 - indexZ;
+            this.node.style.zIndex = zIndex;
             this.node.style.opacity = opacity;
 
         };
